43aabbdcb4b1fb0ff3331e9b1ef2e2d10997a752
David Ronk <dronk@compendium.com>
2018-11-25 19:51:33 -0500

    cleanup

--- rent.js
+++ rent.js
@@ -16,38 +16,26 @@ while (true) {
 
   // if there is enough money (with the percentage taken into consideration)
   // then get a new node
-  //if (getNextHacknetNodeCost() <= currentCash) {
   if (hacknet.getPurchaseNodeCost() <= currentCash) {
-    //purchaseHacknetNode();
     hacknet.purchaseNode();
   } else {
   // otherwise, upgrade the nodes where possible
-    //for (i = 0; i < hacknetnodes.length; i++) {
     for (i = 0; i < hacknet.numNodes(); i++) {
-      // looks like the node = isn't valid with API
-      //node = hacknetnodes[i];
-
-      //upgradeCost = node.getLevelUpgradeCost(1);
       upgradeCost = hacknet.getLevelUpgradeCost(i, 1);
 
       if (upgradeCost <= currentCash) {
-        //node.upgradeLevel(1);
         hacknet.upgradeLevel(i, 1);
         break;
       } else {
-        //ramCost = node.getRamUpgradeCost();
         ramCost = hacknet.getRamUpgradeCost(i, 1);
 
         if (ramCost <= currentCash) {
-          //node.upgradeRam();
           hacknet.upgradeRam(i, 1);
           break;
         } else {
-          //coreCost = node.getCoreUpgradeCost();
           coreCost = hacknet.getCoreUpgradeCost(i, 1);
 
           if (coreCost <= currentCash) {
-            //node.upgradeCore();
             hacknet.upgradeCore(i, 1);
             break;
           }
